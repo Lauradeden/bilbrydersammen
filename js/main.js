89224dda4fd7a295b65ba5b98a0c1e70c912656a
Laura von Deden <lavo0002@stud.ek.dk>
2025-11-20 14:45:52 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -31,29 +31,29 @@ hotspots.forEach((hotspot) => {
 });
 
 function resetInfo() {
-  title.textContent = "Roadtrip Red Alert";
+  title.textContent = "ROADTRIP RED ALERT";
   placeholder.innerHTML = "<p>Tryk på et hotspot (mobil, bil eller bagagerum) for at få fif, grin og hjælp til din nødsituation.</p>";
-  efficiency.innerHTML = "<h2>Godt at vide</h2>";
-  requirement.innerHTML = "<h2>Bonus tip</h2>";
+  efficiency.innerHTML = "<h3>Godt at vide</h3>";
+  requirement.innerHTML = "<h3>Bonus tip</h3>";
 }
 
 function updateInfoboxMobil() {
   title.textContent = "📱 Mobiltelefon - Når signalet svigter";
   placeholder.innerHTML = "<p>Ingen dækning? Gå mod højere grund, men bliv tæt på bilen. Brug strømmen med omtanke - ingen memes, før hjælp er på vej.</p>";
-  efficiency.innerHTML = "<h2>🔋 Spar på strømmen</h2><p>Skru ned for lysstyrken, luk apps - og glem Tiktok for en stund";
-  requirement.innerHTML = "<h2>📞 Nødopkald</h2><p>112 virker uden simkort og netværk. Men ikke hvis du står midt i en tunnel!</p>";
+  efficiency.innerHTML = "<h3>🔋 Spar på strømmen</h3><p>Skru ned for lysstyrken, luk apps - og glem Tiktok for en stund";
+  requirement.innerHTML = "<h3>📞 Nødopkald</h3><p>112 virker uden simkort og netværk. Men ikke hvis du står midt i en tunnel!</p>";
 }
 
 function updateInfoboxBil() {
   title.textContent = "🚗 Bilen - Den dovne helt på landevejen";
   placeholder.innerHTML = "<p>Kør ind til siden, tænd katastrofeblink og sæt advarselstrekanten 50 meter bag bilen.</p>";
-  efficiency.innerHTML = "<h2>⚠️ Bliv synlig</h2><p>Refleksvest på! Du skal ses, ikke stresses.</p>";
-  requirement.innerHTML = "<h2>🔧 Tjek motoren?</h2><p>Hvis du ikke ved, hvad du kigger på under motorhjelmen, så lad den blive lukket.</p>";
+  efficiency.innerHTML = "<h3>⚠️ Bliv synlig</h3><p>Refleksvest på! Du skal ses, ikke stresses.</p>";
+  requirement.innerHTML = "<h3>🔧 Tjek motoren?</h3><p>Hvis du ikke ved, hvad du kigger på under motorhjelmen, så lad den blive lukket.</p>";
 }
 
 function updateInfoboxBagagerum() {
   title.textContent = "🎒 Bagagerummet - Din overlevelsesststion";
   placeholder.innerHTML = "<p>Hvem sagde nødsituation? Du har snacks, tæppe og powerbank - du er klar til alt (næsten).</p>";
-  efficiency.innerHTML = "<h2>🍫 Snack-strategi</h2><p>Del med omtanke - det er nødproviant, ikke roadtrip-buffet.";
-  requirement.innerHTML = "<h2>🧃 Hydration hero</h2><p>Hav altid lidt vand i bagagerummet - kaffe tæller ikke, desværre.";
+  efficiency.innerHTML = "<h3>🍫 Snack-strategi</h3><p>Del med omtanke - det er nødproviant, ikke roadtrip-buffet.";
+  requirement.innerHTML = "<h3>🧃 Hydration hero</h3><p>Hav altid lidt vand i bagagerummet - kaffe tæller ikke, desværre.";
 }
